f04bc0ce70ab7fbe6c99ebf6542aef77b7c55c2c
makbari.it <makbari.it@outlook.com>
2019-01-30 10:11:57 +0330

    init

---
+++ Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Publisher";
+
+            var connection = ConnectionMultiplexer.Connect("192.168.20.82:6379,abortConnect=false,syncTimeout=3000");
+
+            var publisher = connection.GetSubscriber();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var count = publisher.Publish("test", $"Message {i+1}");
+                Thread.Sleep(10000);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Subscriber";
+
+            var connection = ConnectionMultiplexer.Connect("192.168.20.82:6379,abortConnect=false,syncTimeout=3000");
+
+            var subscriber = connection.GetSubscriber();
+
+            subscriber.Subscribe("test", (channel, message) => {
+                Console.WriteLine("Got notification: " + message);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
